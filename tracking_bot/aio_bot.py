7c181dc29ae19ac6d3c285bcc2652d44dab50487
nobless-21 <alexandr.sukhorukov2005@gmail.com>
2024-07-04 12:13:01 +0300

    d

--- tracking_bot/aio_bot.py
+++ tracking_bot/aio_bot.py
@@ -1,27 +1,43 @@
-import aiogram
+from aiogram import Bot, Dispatcher, executor
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-from aiogram import Bot, Dispatcher, executor, types
-
-TOKEN_API = '7250507637:AAHhLqUtN0EZWz9Nd0ojpywHrfZ5FrRWW6A'
+import config
+import keyboards
+storage = MemoryStorage()
 
 HELP_COMMAND = """
 /help - список команд
 /start - начать работу с ботом
+/add - добавляет новую задачу
+/delete - удаляет задачу
 """
 
-bot = Bot(TOKEN_API)
-dp = Dispatcher(bot)
+bot = Bot(token=config.token)
+dp = Dispatcher(bot, storage=storage)
+
+class Form(StatesGroup):
+    name = State()
 
 
-@dp.message_handler(commands=['start'])
-async def echo(message: types.Message):
-    await message.answer(text="Добро пожаловать в мой телеграмм бот!")
-    await message.delete()
+@dp.message_handler(commands='start')
+async def start(message: types.Message, state: FSMContext):
+    await message.answer('Здравствуйте!\nВы попали в сервис отслеживания личного расписания!\nКак я могу к вам обращаться?')
+    date = message.date
+    await Form.name.set()
+
+@dp.message_handler(state=Form.name)
+async def set_name(message: types.Message, state: FSMContext):
+    name = message.text
+    await message.answer(f'Приятно познакомиться, {name}!')
+    await state.finish()
+
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
     await message.answer(text=HELP_COMMAND)
 
-
-if __name__ == "__main__":
-    executor.start_polling(dp)
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
 
